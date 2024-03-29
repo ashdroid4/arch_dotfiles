383126131c29e2422f6fd9703cf3d9b3384e18f8
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2024-03-29 12:52:43 +0530

    Update install.py

--- install.py
+++ install.py
@@ -9,20 +9,20 @@ from sys import exit
 
 
 #-------------------------------CONSTANTS----------------------------------#
-repoList = """
-1. linuxmobile_hyprland-dotfiles"""
+repoList = { 
+"linuxmobile_hyprland-dots":"https://github.com/linuxmobile/hyprland-dots"
+}
 #--------------------------------------------------------------------------#
 
 
-#-----------------------------Script---------------------------#
+#-----------------------------Script Start---------------------------#
 
+print("Welcome! This will help you setup your Window Manager for your Arch Linux.\n"
+      "PS: You can always abort the script by using Ctrl + C")
 
 # This function will determine the input as True or False.
 def yon(arg):
     return True if "y" in arg else False
-
-print("Welcome! This will help you setup your Window Manager for your Arch Linux.\n"
-      "PS: You can always abort the script by using Ctrl + C")
 
 archInstalled = yon(input("\nHave you installed base package for Arch Linux and set it up?\n"
                           "Yes or No: "))
@@ -32,5 +32,21 @@ if not archInstalled:
           "Come back after setting up Arch.")
     exit()
 
-print("\nCool. Now, tell me which one do you choose?")
-repoChoice = input(repoList)
+# This function will help choose which repo to go for.
+def repoChoose():
+    n=1; print("\nCool. Now, tell me which one do you choose?")
+    for i in repoList:
+        print(f"\n{n}. {i}({repoChoose[i]})"); n+=1
+        
+    while True:
+        repoChoose = input("\nSo, which one do you choose to install? Say in numbers: ")
+        if not repoChoose.isdigit(): print("Please, type a number.")
+        if 0 < int(repoChoose) > n: print("Please put a valid number.")
+        break
+
+    templist = [i for i in repoList]
+    repoChoose = repoList[repoChoose - 1]
+
+    print("Cool, so you choose ", repoChoose)
+
+#-----------------------------Script End-----------------------------#
