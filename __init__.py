ea64e3d5f3c88e7e8aeb0caed24816dfd007f6df
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2024-04-17 16:15:27 +0530

    Updated keyserver for Chaotic-AUR in __init__.py

--- __init__.py
+++ __init__.py
@@ -99,7 +99,7 @@ def run(arg:str, possible_warning=""):
 
 chaoticScript = """
 pacman-key --init
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --recv-key 3056513887B78AEB 
 pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'"""
